2c8c137178fa0f947a7baeed6dc9b6487657c555
mabel <bigbelk@live.ru>
2021-10-13 17:01:39 +0300

    welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,26 +1,8 @@
 export default function(){
     return (
         <>
-           <h3>Content</h3>
-
-            <p>Welcome bij de Web3React Webshop</p>
-            <p>De volgende elementen uit de lessen moeten anwezig zijn in het project:</p>
-            <ul id="lstElementen">
-                <li>Rest API Node.js(Express) server</li>
-                <li>MySQL Databank</li>
-                <li>React Router</li>
-                <li>React Hooks</li>
-                <li>Redux</li>
-                <li>(Formic) *</li>
-                <li>(Frontend framework - Material UI, Bootstrap, Tallwindcss, ...)*</li>
-                <li>React</li>
-            </ul>
-            <div className="myDIV">
-                <p>Optioneel toe te voegen aan het project</p>
-            </div>
-            <div className="styleBold">
-                <p>De deadline om het project in te dienen ligt op 5 Januari 2021 tegen 23h59</p>
-            </div>
+            <h3>Добро пожаловать!</h3>
+            <p>Это простой пример фронтенда на React/Redux.</p>
         </>
     )
 }
